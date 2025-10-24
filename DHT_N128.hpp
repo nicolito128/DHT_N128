de1575b6b66642492468f58db8c927a9e2e81bcb
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2025-10-24 01:15:17 -0300

    first 'working' implementation: correct reading of bit stream

--- DHT_N128.hpp
+++ DHT_N128.hpp
@@ -1,12 +1,10 @@
 #ifndef DHT_N128_H_
 #define DHT_N128_H_
-#define DEFAULT_PULL_TIME 40
+#define DEFAULT_PULL_TIME 50
 
 #include <Arduino.h>
 
-typedef int32_t raw_magnitude;
-
-typedef float magnitude;
+typedef uint32_t raw_magnitude;
 
 enum DHT_Type {
   DHT11,
@@ -23,19 +21,21 @@ public:
 private:
   enum DHT_Type _typ;
 
+  bool _bits[40];
+
   int _pin;
 
   uint8_t _pullTime, _data[5];
 
-  int32_t _raw;
+  raw_magnitude _raw;
 
-  unsigned long _lastreadTime, _maxcycles;
+  unsigned long _maxcycles;
 
   void _startSignal();
 
   bool _readBit();
 
-  uint32_t _awaitPulse(bool);
+  uint32_t _awaitPulse(int);
 
   void _clear();
 };
